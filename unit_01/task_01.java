e937a2aa005edaf957835d1ae6cf37e0607b6f73
RV0225 <83663322+RV0225@users.noreply.github.com>
2022-03-24 22:43:53 +0530

    first commit

--- unit_01/task_01.java
+++ unit_01/task_01.java
@@ -1,11 +1,18 @@
 package unit_01;
 
 public class task_01 {
-    static final int a = 10;
-    public static final float pi = 3.14f;
-
-    public static void main(String[] args) {
-        System.out.println(a);
-        System.out.println(pi);
+    class A {
+        int b;
     }
+
+public static void main(String args[]){
+    //Syntax error on token "float" ,invalid variableDeclaratorId
+    int float =-2.5f;
+    //A cannot be resolved to a variable
+    //Syntax error on token "int", delete this token
+    A int =new A;
+}
+    // Syntax error on token "double", Identifier expected after this token
+    double();
+
 }
