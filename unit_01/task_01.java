5ea80aefffa838920f24db3414478473040f90e2
RV0225 <83663322+RV0225@users.noreply.github.com>
2022-03-24 22:17:51 +0530

    first commit

---
+++ unit_01/task_01.java
@@ -0,0 +1,11 @@
+package unit_01;
+
+public class task_01 {
+    static final int a = 10;
+    public static final float pi = 3.14f;
+
+    public static void main(String[] args) {
+        System.out.println(a);
+        System.out.println(pi);
+    }
+}
