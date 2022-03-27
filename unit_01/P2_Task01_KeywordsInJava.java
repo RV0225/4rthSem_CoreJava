1d1c502d4d99f1eacb4098993cab175370cbefac
RV0225 <83663322+RV0225@users.noreply.github.com>
2022-03-27 08:39:30 +0530

    commit

--- unit_01/P2_Task01_KeywordsInJava.java
+++ unit_01/P2_Task01_KeywordsInJava.java
@@ -5,14 +5,14 @@ public class P2_Task01_KeywordsInJava {
         int b;
     }
 
-public static void main(String[] args){
-    //Syntax error on token "float" ,invalid variableDeclaratorId
-    int float =-2.5f;
-    //A cannot be resolved to a variable
-    //Syntax error on token "int", delete this token
-    A int =new A;
-}
+    public static void main(String[] args) {
+        // Syntax error on token "float" ,invalid variableDeclaratorId
+        // int float =-2.5f;
+        // A cannot be resolved to a variable
+        // Syntax error on token "int", delete this token
+        // A int =new A;
+    }
     // Syntax error on token "double", Identifier expected after this token
-    double();
+    // double();
 
 }
