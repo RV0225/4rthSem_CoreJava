7def4d85ab7c65c67f2b267b60429483596d33f4
RV0225 <83663322+RV0225@users.noreply.github.com>
2022-03-27 08:08:49 +0530

    Merge branch 'master' of https://github.com/RV0225/Java_4rthSem

---
+++ unit_01/P2_Task02_ConstantsInJava.java
@@ -0,0 +1,13 @@
+package unit_01;
+
+public class P2_Task02_ConstantsInJava {
+    static final double pi = 3.14;
+    public static final double price = 234.84;
+
+    public static void main(String[] args) {
+        final int a = 100;
+        System.out.println("value of pi is " + pi);
+        System.out.println("a = " + a);
+        System.out.println("Price = " + price);
+    }
+}
