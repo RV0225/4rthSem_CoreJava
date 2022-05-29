751630b58f763f0e549544d5b3cfce15694a6b0e
RV0225 <83663322+RV0225@users.noreply.github.com>
2022-05-29 22:08:59 +0530

    c

--- Unit_03/P16_MultiThreading_Multitasking.java
+++ Unit_03/P16_MultiThreading_Multitasking.java
@@ -10,6 +10,32 @@ public class P16_MultiThreading_Multitasking {
 
         t1.start();
         t.start();
+        // C1 c1 = new C1();
+        // C2 c2 = new C2();
+        // c1.show1();
+        // c2.show2();
+    }
+}
+
+class C1 {
+    void show1() {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x);
+        System.out.println("inside show1");
+        sc.close();
+    }
+}
+
+class C2 {
+    void show2() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
     }
 }
 
@@ -24,10 +50,10 @@ class T1 extends Thread {
 
 class T2 extends Thread {
     public void run() {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 1; i <= 5; i++) {
             System.out.println(i);
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
             }
         }
